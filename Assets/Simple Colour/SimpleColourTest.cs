737a12f8f55378b2a1f82e9dbab6c390b7e80b69
Duane Bradbury <dibely@hotmail.com>
2018-12-01 21:02:53 +0000

    Added a simple colour scene with a compute shader and script that sets a textures colour to red.

---
+++ Assets/Simple Colour/SimpleColourTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SimpleColourTest : MonoBehaviour {
+    private const int TEXTURE_SIZE = 256;
+    public ComputeShader computeShader;
+    private RenderTexture renderTexture;
+
+    private void Awake() {
+        CreateRenderTexture();
+
+        Renderer renderer = GetComponent<Renderer>();
+        Material material = renderer.material;
+        material.SetTexture("_MainTex", renderTexture);
+        UpdateTextureFromComputeShader();
+    }
+
+    private void CreateRenderTexture() {
+        renderTexture = new RenderTexture(TEXTURE_SIZE, TEXTURE_SIZE, 24);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
+    }
+
+    private void OnDestroy() {
+        renderTexture.Release();
+        renderTexture = null;
+    }
+
+    void Update() {
+
+    }
+
+    private void UpdateTextureFromComputeShader() {
+        int kernelHandle = computeShader.FindKernel("CSMain");
+
+        computeShader.SetTexture(kernelHandle, "Result", renderTexture);
+        computeShader.Dispatch(kernelHandle, TEXTURE_SIZE / 8, TEXTURE_SIZE / 8, 1);
+    }
+}
