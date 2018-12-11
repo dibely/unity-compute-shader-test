636fd203649a30807431e34c212b793e1a502a7b
Duane Bradbury <dibely@hotmail.com>
2018-12-11 11:08:42 +0000

    Refactored the code as follows: * Renamed properties to be more readable and relevant to what they are * Removed commented out code * Added defines/constants

--- Assets/Average Colour/AverageColour.cs
+++ Assets/Average Colour/AverageColour.cs
@@ -2,6 +2,11 @@
 
 public class AverageColour : MonoBehaviour {
     private const int TEXTURE_SIZE = 32;
+    private const int THREAD_GROUP_X = TEXTURE_SIZE / 8;
+    private const int THREAD_GROUP_Y = TEXTURE_SIZE / 8;
+    private const int THREAD_COUNT_X = 4;
+    private const int THREAD_COUNT_Y = 4;
+
     public ComputeShader computeShader;
     public Texture2D sourceTexture;
     private RenderTexture renderTexture;
@@ -33,18 +38,14 @@ public class AverageColour : MonoBehaviour {
     private void CalculateAverage() {
         // The array size is the total number of threads that will be executed.  
         // The value comes from the parameters for the Dispatch call multiplied with the numthreads specified in the compute shader. 
-        int arraySize = 4 * 4 * 4 * 4;
+        int arraySize = THREAD_GROUP_X * THREAD_COUNT_X * THREAD_GROUP_Y * THREAD_COUNT_Y;
         floatBuffer = new ComputeBuffer(arraySize, sizeof(float)*3, ComputeBufferType.Default);
 
         int kernelHandle = computeShader.FindKernel("CSMain");
-        computeShader.SetInt("TextureSize", TEXTURE_SIZE);
         computeShader.SetTexture(kernelHandle, "RenderTexture", renderTexture);
         computeShader.SetTexture(kernelHandle, "SourceTexture", sourceTexture);
         computeShader.SetBuffer(kernelHandle, "FloatBuffer", floatBuffer);
-        computeShader.Dispatch(kernelHandle, TEXTURE_SIZE / 8, TEXTURE_SIZE / 8, 1);
-        //computeShader.Dispatch(kernelHandle, 1, 1, 1);
-        //computeShader.Dispatch(kernelHandle, 1, 1, 1);
-        //computeShader.Dispatch(kernelHandle, 1, 1, 1);
+        computeShader.Dispatch(kernelHandle, THREAD_GROUP_X, THREAD_GROUP_Y, 1);
 
         Vector3[] result = new Vector3[arraySize];
         floatBuffer.GetData(result);
