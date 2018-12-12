15fd7e284d6255504924f6a15604d78e6db7937a
Duane Bradbury <dibely@hotmail.com>
2018-12-12 14:31:02 +0000

    Updated the simple polygon rendering to use an append buffer where the compute shader will add 6 vertices to form a quad from two triangles of 3 vertices.  As part of this updated the DrawProcedural call to use DrawProceduralIndirect instead with an argument buffer and switched the topology to Triangles.

--- Assets/Simple Polygon/SimplePolygon.cs
+++ Assets/Simple Polygon/SimplePolygon.cs
@@ -9,15 +9,16 @@ public class SimplePolygon : MonoBehaviour {
         public Vector2 uv;
     }
 
-    private const int VERTEX_COUNT = 4;
-    private const int THREAD_COUNT_X = 2; // Must match numthreads parameter in the compute shader
-    private const int THREAD_COUNT_Y = 2; // Must match numthreads parameter in the compute shader
-    private const int THREAD_GROUP_X = VERTEX_COUNT / (THREAD_COUNT_X * THREAD_COUNT_Y);
+    private const int VERTEX_COUNT = 6;
+    private const int THREAD_COUNT_X = 1; // Must match numthreads parameter in the compute shader
+    private const int THREAD_COUNT_Y = 1; // Must match numthreads parameter in the compute shader
+    private const int THREAD_GROUP_X = 1;
     private const int THREAD_GROUP_Y = 1;
 
     public ComputeShader computeShader;
 
     private ComputeBuffer vertexBuffer;
+    private ComputeBuffer argBuffer;
     public Material material;
 
     private void Awake() {
@@ -30,15 +31,22 @@ public class SimplePolygon : MonoBehaviour {
     private void BuildVertexData() {
         // The array size is the total number of threads that will be executed.  
         // The value comes from the parameters for the Dispatch call multiplied with the numthreads specified in the compute shader. 
-        int arraySize = THREAD_GROUP_X * THREAD_COUNT_X * THREAD_GROUP_Y * THREAD_COUNT_Y;
+        int arraySize = THREAD_GROUP_X * THREAD_COUNT_X * THREAD_GROUP_Y * THREAD_COUNT_Y * VERTEX_COUNT;
+        Debug.Log("Array Size: " + arraySize);
 
         Vertex[] vertexArray = new Vertex[arraySize];
-        vertexBuffer = new ComputeBuffer(vertexArray.Length, sizeof(float)*8, ComputeBufferType.Default);
+        vertexBuffer = new ComputeBuffer(vertexArray.Length, sizeof(float)*8, ComputeBufferType.Append);
+        vertexBuffer.SetCounterValue(0);
         vertexBuffer.SetData(vertexArray);
+
+        argBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
+        int[] args = new int[] { 0, 1, 0, 0 };
+        argBuffer.SetData(args);
 
         int kernelHandle = computeShader.FindKernel("CSMain");
         computeShader.SetInt("DispatchSizeX", THREAD_GROUP_X);
         computeShader.SetInt("DispatchSizeY", THREAD_GROUP_Y);
+        computeShader.SetFloat("Size", 2.0f);
 
         computeShader.SetBuffer(kernelHandle, "VertexBuffer", vertexBuffer);
         computeShader.Dispatch(kernelHandle, THREAD_GROUP_X, THREAD_GROUP_Y, 1);
@@ -51,6 +59,15 @@ public class SimplePolygon : MonoBehaviour {
             Debug.Log(data.normal);
             Debug.Log(data.uv);
         }
+
+        ComputeBuffer.CopyCount(vertexBuffer, argBuffer, 0);
+
+        // DEBUG
+        argBuffer.GetData(args);
+        Debug.Log("Vertex count: " + args[0]);
+        Debug.Log("Instance count: " + args[1]);
+        Debug.Log("Start vertex: " + args[2]);
+        Debug.Log("Start instance: " + args[3]);
     }
 
     private void OnRenderObject() {
@@ -70,10 +87,11 @@ public class SimplePolygon : MonoBehaviour {
         //Debug.Log("vertexBuffer count: " + vertexBuffer.count);
         material.SetPass(0);
         material.SetBuffer("vertexBuffer", vertexBuffer);
-        Graphics.DrawProcedural(MeshTopology.Points, vertexBuffer.count);
+        Graphics.DrawProceduralIndirect(MeshTopology.Triangles, argBuffer, 0);
     }
 
     private void OnDestroy() {
         vertexBuffer.Release();
+        argBuffer.Release();
     }
 }
